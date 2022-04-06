e1c2305df54f7136f582cfeaa0fd124319c3aa0e
Abner Santana <abnersant10@gmail.com>
2022-04-05 22:21:50 -0300

    up

--- lladmin/views.py
+++ lladmin/views.py
@@ -1,15 +1,32 @@
 from urllib import request
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
+from django.contrib import messages
+from django.contrib.auth import *
 # Create your views here.
 
 
 def home(request):
+    if request.method == 'POST':
+        cpf = request.POST.get('cpf')
+        senha = request.POST.get('senha')
+        # verrificar se CPF e Senha informada é igual no BD
+        user = authenticate(request, username=cpf, password=senha)
+        if user is not None:
+            login(request, user)
+            return redirect('clientes_cadastro')
+        else:
+            messages.error(request, 'CPF ou Senha Incorreta!')
     return render(request, "home.html")
 
 
+def logout_view(request):
+    logout(request)
+    return redirect('/')
+
+
 def clientes_cadastro(request):
-    pass
+    if request.user.is_authenticated == True:
+        return render(request, "clientes_cadastro.html")
 
 
 def clientes_consulta(request):
--- llsolar/urls.py
+++ llsolar/urls.py
@@ -15,15 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from lladmin.views import home, clientes_cadastro, clientes_consulta, equipamentos_cadastro
+from lladmin.views import home, clientes_cadastro, clientes_consulta, equipamentos_cadastro, logout_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
     # aqui seria a página inicial
-    path('clientes/cadastro', clientes_cadastro, name='clientes_cadastro'),
+    path('/clientes/cadastro', clientes_cadastro, name='clientes_cadastro'),
     path('clientes/consulta', clientes_consulta, name='clientes_consulta'),
     path('equipamentos/cadastro', equipamentos_cadastro,
          name='equipamentos_cadastro'),
+    path('logout', logout_view, name="logout"),
 
 ]
