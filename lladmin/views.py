63096742aa8583c0bc47d0cc4a1e2258a6166938
Abner Santana <abnersant10@gmail.com>
2022-04-05 21:11:56 -0300

    up

--- lladmin/views.py
+++ lladmin/views.py
@@ -1,3 +1,20 @@
+from urllib import request
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home(request):
+    return render(request, "home.html")
+
+
+def clientes_cadastro(request):
+    pass
+
+
+def clientes_consulta(request):
+    pass
+
+
+def equipamentos_cadastro(request):
+    pass
