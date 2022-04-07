c80ca915acd6985fb7fd11281aad86a6d1b44749
Abner Santana <abnersant10@gmail.com>
2022-04-07 12:48:23 -0300

    models create

--- lladmin/models.py
+++ lladmin/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class cliente(models.Model):
+    cpf_cnpj = models.CharField(primary_key=True, max_length=14)
+    tipo_cliente = models.CharField(max_length=4)  # CPF ou CNPJ
+    nome_completo = models.CharField(max_length=150)
+    whatsapp = models.CharField(max_length=11)
+    email = models.EmailField(max_length=100)
+    endereco = models.CharField(max_length=100)
+    numero = models.CharField(max_length=5)
+    bairro = models.CharField(max_length=100)
+    cidade = models.CharField(max_length=100)
+    estado = models.CharField(max_length=2)
+    complemento = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.cpf_cnpj
+
+
+class equipamento(models.Model):
+    tipo = models.CharField(max_length=20)
+    descricao = models.CharField(max_length=150)
+    fabricante = models.CharField(max_length=100)
+    modelo = models.CharField(max_length=100)
+    potencia = models.IntegerField()
+
+    def __str__(self):
+        return self.descricao
