e56637fc57f961441655510bf4c8bb368caa2a03
Abner Santana <abnersant10@gmail.com>
2022-05-02 16:00:52 -0300

    5

--- llsolar/urls.py
+++ llsolar/urls.py
@@ -33,4 +33,4 @@ urlpatterns = [
     path('logout', logout_view, name="logout"),
 
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]  # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
